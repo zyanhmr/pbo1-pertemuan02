1c398c7ab71e4d3c5a71951158d654a90eeb76a2
zyanhmr <zyan.humaira@gmail.com>
2024-04-19 23:29:12 +0800

    update input output

--- InputOutput.java
+++ InputOutput.java
@@ -10,12 +10,10 @@ public class InputOutput{
   System.out.print("Masukkan nama anda: ");
   nama = scanner.nextLine();
   System.out.println("Hai, "+nama);
-
-  
+ 
   System.out.print("Masukkan jumlah SKS yang sudah ditempuh: ");
   jumlahSks = scanner.nextInt();
 
-  
   System.out.print("Masukkan IPK Sementara: ");
   IPK = scanner.nextDouble();
 
